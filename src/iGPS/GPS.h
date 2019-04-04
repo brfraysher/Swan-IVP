e8d9806e98e4756080bbc1ec8875ea9ccb6a265a
Andrew Sucato <ansucato@crimson.ua.edu>
2019-04-04 14:59:00 -0700

    Generate iGPS files

---
+++ src/iGPS/GPS.h
@@ -0,0 +1,36 @@
+/************************************************************/
+/*    NAME: Andrew Sucato                                              */
+/*    ORGN: MIT                                             */
+/*    FILE: GPS.h                                          */
+/*    DATE: December 29th, 1963                             */
+/************************************************************/
+
+#ifndef GPS_HEADER
+#define GPS_HEADER
+
+#include "MOOS/libMOOS/Thirdparty/AppCasting/AppCastingMOOSApp.h"
+
+class GPS : public AppCastingMOOSApp
+{
+ public:
+   GPS();
+   ~GPS();
+
+ protected: // Standard MOOSApp functions to overload  
+   bool OnNewMail(MOOSMSG_LIST &NewMail);
+   bool Iterate();
+   bool OnConnectToServer();
+   bool OnStartUp();
+
+ protected: // Standard AppCastingMOOSApp function to overload 
+   bool buildReport();
+
+ protected:
+   void registerVariables();
+
+ private: // Configuration variables
+
+ private: // State variables
+};
+
+#endif 
