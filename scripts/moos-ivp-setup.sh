714bcc20b0736846e502e23f70002b83654d5953
Andrew Sucato <ansucato@crimson.ua.edu>
2019-04-08 08:05:48 -0700

    Check for svn

--- scripts/moos-ivp-setup.sh
+++ scripts/moos-ivp-setup.sh
@@ -10,7 +10,7 @@ echo 'Updating Repository'
 apt update
 
 echo 'Cloning moos-ivp tree'
-svn co https://oceanai.mit.edu/svn/moos-ivp-aro/trunk/ ~/moos-ivp
+svn co https://oceanai.mit.edu/svn/moos-ivp-aro/trunk/ ~/moos-ivp || { echo 'ERROR: Run sudo apt install subversion' ; exit 1; }
 
 echo 'Installing dependencies'
 apt install -y g++  cmake  xterm || { echo 'Error in first apt install' ; exit 1; }
