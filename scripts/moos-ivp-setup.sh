da4a6288668967bba6c47e73ce429de709dcbd8b
Andrew Sucato <ansucato@crimson.ua.edu>
2019-04-08 07:45:38 -0700

    Add moos-ivp-setup script

---
+++ scripts/moos-ivp-setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+repoDir=$(pwd)
+
+echo 'Entering home directory'
+cd ~/
+
+echo 'Updating Repository'
+apt update
+
+echo 'Cloning moos-ivp tree'
+svn co https://oceanai.mit.edu/svn/moos-ivp-aro/trunk/ ~/moos-ivp
+
+echo 'Installing dependencies'
+apt install -y g++  cmake  xterm || { echo 'Error in first apt install' ; exit 1; }
+apt install -y libfltk1.3-dev  freeglut3-dev  libpng-dev  libjpeg-dev || { echo 'Error in second install' ; exit 1; }
+apt install -y libxft-dev  libxinerama-dev   libtiff5-dev || { echo 'Error in third install' ; exit 1; }
+
+echo 'Entering moos-ivp'
+cd ~/moos-ivp/
+
+echo 'Building moos'
+source build-moos.sh || { echo 'Error building moos' ; exit 1; }
+
+echo 'Building ivp modules'
+source build-ivp.sh || { echo 'Error building Ivp modules' ; exit 1; }
+
+echo 'Entering repository directory'
+cd $repoDir
+
+echo 'Building repository modules'
+source build.sh || { echo 'Error building repository modules' ; exit 1; }
+
+echo 'Adding moos-ivp binaries path to .bashrc file'
+echo "export PATH=$PATH:~/moos-ivp/bin:$repoDir/bin"
+
+echo "Sourcing bash"
+
+
+
+
+
