7d02993e0f72a8c9c97f6c831996e90430c385fd
Andrew Sucato <ansucato@crimson.ua.edu>
2019-08-27 19:53:38 -0500

    Update setup script to install serial library

--- scripts/moos-ivp-setup.sh
+++ scripts/moos-ivp-setup.sh
@@ -15,10 +15,35 @@ sudo apt update -y
 echo "---Cloning moos-ivp tree---"
 svn co https://oceanai.mit.edu/svn/moos-ivp-aro/trunk/ ~/moos-ivp || { echo "---Error cloning moos-ivp tree... Is subversion installed?---" ; exit 1; }
 
-echo "---Installing dependencies---"
+echo "---Installing moos dependencies---"
 sudo apt install -y g++  cmake  xterm || { echo "---Error in first apt install---" ; exit 1; }
 sudo apt install -y libfltk1.3-dev  freeglut3-dev  libpng-dev  libjpeg-dev || { echo "---Error in second install---" ; exit 1; }
 sudo apt install -y libxft-dev  libxinerama-dev   libtiff5-dev || { echo "---Error in third install---" ; exit 1; }
+
+### Install wjwwood/serial library from git
+echo "---Downloading catkin source---"
+git clone https://github.com/ros/catkin || { echo "---Error downloading catkin source---" ; exit 1;  }
+
+echo "---Installing catkin dependencies---"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu `lsb_release -sc` main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add - || { echo "---Error adding ROS repo---" ; exit 1; }
+sudo apt install -y cmake python-catkin-pkg python-empy python-nose python-setuptools libgtest-dev build-essential || { echo "---Error installing catkin dependencies---" ; exit 1; }
+
+echo "---Moving into catkin directory---"
+cd ~/catkin
+
+echo "---Building and installing catkin---"
+mkdir build && cd build && cmake ../ && make && sudo make install || { echo "---Error building and installing catkin---" ; exit 1; }
+
+echo "---Cloning serial repository---"
+cd ~
+git clone https://github.com/wjwwood/serial.git || { echo "---Error cloning serial git repository---" ; exit 1; }
+
+echo "---Building and installing serial library---"
+cd serial
+cmake -DCMAKE_INSTALL_PREFIX=/usr/local -DBUILD_SHARED_LIBS=OFF
+make || { echo "---Error building serial library---" ; exit 1; }
+sudo make install || { echo "---Error installing serial library---" ; exit 1; }
 
 echo "---Entering moos-ivp---"
 cd ~/moos-ivp/
@@ -37,6 +62,11 @@ echo "---Building repository modules---"
 
 echo "---Adding moos-ivp binaries path to .bashrc file---"
 echo "export PATH=$PATH:~/moos-ivp/bin:$repoDir/bin" >> ~/.bashrc
+
+### Clean Up ###
+echo "---Cleaning up!---"
+cd ~
+sudo rm -rf serial/ catkin/
 
 echo "---Success!!!---"
 echo ""
